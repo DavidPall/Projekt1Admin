31eebf74b1d40cedd67bd9c6712fe644ae673c25
David Pall <davidpallatx@gmail.com>
2019-11-23 13:59:55 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/projekt1admin/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.projekt1admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    var isFragmentOneLoaded = true
+    val manager = supportFragmentManager
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ShowFragmentLogin()
+
+    }
+
+    fun ShowFragmentLogin() {
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentLogin()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = true
+    }
+
+    fun ShowFragmentEdit() {
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentEdit()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = false
+    }
+
+    fun ShowFragmentAnswers() {
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentAnswers()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = false
+    }
+}
